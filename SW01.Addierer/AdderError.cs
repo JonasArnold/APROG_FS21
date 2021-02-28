646782fec60a5741bf10d6569ec51a838e5931c3
Jonas Arnold <jonas.arnold@live.de>
2021-02-28 15:23:21 +0100

    homework adder

---
+++ SW01.Addierer/AdderError.cs
@@ -0,0 +1,15 @@
+﻿namespace SW01.Adder
+{
+  internal enum AdderError:int
+  {
+    /// <summary>
+    /// Error OK => No error happened
+    /// </summary>
+    OK = 0,
+
+    /// <summary>
+    /// No Integer Number could be parsed from Arguments.
+    /// </summary>
+    NoNumberParsedFromArgs = -11,
+  }
+}
---
+++ SW01.Addierer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SW01.Adder
+{
+  class Program
+  {
+    static int Main(string[] args)
+    {
+      AdderError error = AdderError.OK;
+
+      // sum up all args
+      int sum = 0;
+      foreach (string arg in args)
+      {
+        // try parse int value
+        if (int.TryParse(arg, out int parsedArg))
+        {
+          sum += parsedArg; // add to sum
+          Console.WriteLine($"Added value={parsedArg}. sum={sum}");
+        }
+        // else no number => ignore
+      }
+
+      // check if sum is 0 (no numbers recoginzed in args)
+      if (sum == 0)
+      {
+        error = AdderError.NoNumberParsedFromArgs;
+        Console.WriteLine("No integer Number could be parsed from the arguments.");
+      }
+      else // if sum is not 0 => display result
+      {
+        Console.WriteLine($"Calculated sum: {sum}");
+      }
+
+      return (int)error;
+    }
+  }
+}
