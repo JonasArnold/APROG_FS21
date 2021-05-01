cae7f1621744bcd4aec510fc0c645307a288448f
Jonas Arnold <jonas.arnold@live.de>
2021-05-01 10:11:33 +0200

    added DayTime Client

---
+++ SW09.Networking/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SW09.Networking
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Starting TCP DayTime Client...");
+
+      TcpDayTimeClient tcpDayTimeClient = new TcpDayTimeClient();
+      tcpDayTimeClient.Connect();
+      Console.WriteLine("Connected.");
+
+      Console.WriteLine($"ReadLine: {tcpDayTimeClient.ReadLine()}");
+
+      tcpDayTimeClient.Close();
+      Console.WriteLine("Closed Connection.");
+    }
+  }
+}
---
+++ SW09.Networking/TcpDayTimeClient.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+namespace SW09.Networking
+{
+  internal class TcpDayTimeClient
+  {
+    private const string serverHostname = "time.nist.gov";
+    private const int serverPort = 13;
+
+    private readonly TcpClient tcpClient;
+    private Socket socket;
+
+    public TcpDayTimeClient()
+    {
+      this.tcpClient = new();
+    }
+    public void Connect()
+    {
+      this.tcpClient.Connect(serverHostname, serverPort);
+      this.socket = this.tcpClient.Client;
+    }
+
+    public string ReadLine()
+    {
+      if(this.socket == null)
+      {
+        return string.Empty;
+      }
+
+      string line = "";
+      using(StreamReader sr = new(tcpClient.GetStream()))
+      {
+        // read as lines until it is not empty anymore
+        do
+        {
+          line = sr.ReadLine();
+        } while (line == "");
+      }
+      return line;
+    }
+
+    public void Close()
+    {
+      this.tcpClient.Close();
+    }
+  }
+}
