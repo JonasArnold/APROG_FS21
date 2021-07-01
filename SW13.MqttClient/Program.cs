b8c7be8977fd8c095dee66728dc75ee04b9a76ad
Jonas Arnold <jonas.arnold@live.de>
2021-07-01 11:23:25 +0200

    added publish to Mqtt Client

--- SW13.MqttClient/Program.cs
+++ SW13.MqttClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
@@ -9,9 +10,9 @@ namespace SW13.MqttClient
   {
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello MQTT World!");
+			Console.WriteLine("Hello MQTT World! Press Space to send test message to topic 'scada/status'.");
 			// creating an MqttClient object
-			var client = new uPLibrary.Networking.M2Mqtt.MqttClient("broker.hivemq.com");
+			var client = new uPLibrary.Networking.M2Mqtt.MqttClient("192.168.1.161");
 			// register to message received
 			client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
 			// generate a clientID and connect to Broker
@@ -22,6 +23,16 @@ namespace SW13.MqttClient
 			// Endless loop
 			while (true)
 			{
+				Thread.Sleep(50);
+				if(Console.KeyAvailable)
+        {
+					var key = Console.ReadKey();
+					if(key.Key == ConsoleKey.Spacebar)
+          {
+            Console.WriteLine($"Sending \"Test Message\" to topic 'scada/status'");
+						client.Publish("scada/status", Encoding.ASCII.GetBytes("Test Message"));
+          }
+        }
 			}
 		}
 		static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
